6bd288b145c031190fb522b2e2a9c687769b753b
Alexandru Balan <61700567+Al3xBM@users.noreply.github.com>
2021-11-16 12:55:37 +0200

    Tryout project
    Created a project that pulls movies and their genres from dbpedia
    Added it here so that others can see how it works/should work

---
+++ DBpediaComm/DBpediaComm/Controllers/TryoutController.cs
@@ -0,0 +1,53 @@
+﻿using DBpediaComm.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using VDS.RDF;
+using VDS.RDF.Query;
+
+namespace DBpediaComm.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TryoutController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> GetResults()
+        {
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"));
+            endpoint.ResultsAcceptHeader = "application/sparql-results+json";
+
+            SparqlResultSet results = endpoint.QueryWithResultSet(@"    
+                    PREFIX omc: <omc.ttl>
+                    PREFIX cw: <cw.ttl>
+                
+                    SELECT * WHERE {
+                        ?movie a dbo:Film .
+                        ?movie a schema:CreativeWork .
+                        ?movie dbo:genre ?genre . 
+                    } LIMIT 100");
+
+            Dictionary<string, List<string>> movieGenres = new Dictionary<string, List<string>>();
+
+            foreach(var result in results)
+            {
+                var title = result.Value("movie").ToString();
+                var genre = result.Value("genre").ToString();
+                if (!movieGenres.ContainsKey(title))
+                    movieGenres[title] = new List<string>();
+
+                movieGenres[title].Add(genre);
+            }
+
+            List<string> output = results.ToList().Select(x => x.ToString()).ToList();
+
+            TryoutCreateFile.CreateFile(movieGenres);
+            TryoutCreateFile.QueryFile();
+
+            return output;
+        }
+    }
+}
---
+++ DBpediaComm/DBpediaComm/Helpers/TryoutCreateFile.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VDS.RDF;
+using VDS.RDF.Ontology;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Query.Datasets;
+using VDS.RDF.Writing;
+
+namespace DBpediaComm.Helpers
+{
+    public class TryoutCreateFile
+    {
+        public static void CreateFile(Dictionary<string, List<string>> movieGenres)
+        {
+            /*Graph g = new Graph();
+
+            IUriNode film = g.CreateUriNode(UriFactory.Create()
+            IUriNode hasGenre = g.CreateUriNode(UriFactory.Create("http://example.org/hasGenre"));
+            foreach (string title in movieGenres.Keys)
+            {
+                IUriNode myRdf = g.CreateUriNode(UriFactory.Create(title));
+                foreach (string genre in movieGenres[title])
+                {
+                    ILiteralNode genreNode = g.CreateLiteralNode(genre);
+                    g.Assert(new Triple(myRdf, hasGenre, genreNode));
+                }
+            }*/
+
+            OntologyGraph g = new OntologyGraph();
+            var filmClass = g.CreateOntologyClass(UriFactory.Create("http://dbpedia.org/ontology/Film"));
+            var genreClass = g.CreateOntologyClass(UriFactory.Create("http://dbpedia.org/ontology/genre"));
+            var a = g.CreateUriNode("rdf:type");
+            var prop = g.CreateUriNode("rdf:Property");
+            
+            foreach (string title in movieGenres.Keys)
+            {
+                var film = g.CreateOntologyClass(UriFactory.Create(title));
+                film.AddType(filmClass);
+                // g.Assert(new Triple(film, a, filmClass));
+                foreach (string genreStr in movieGenres[title])
+                {
+                    var genre = g.CreateOntologyClass(UriFactory.Create(genreStr));
+                    genre.AddType(genreClass);
+
+                    g.Assert(new Triple(film.Resource, genreClass.Resource, genre.Resource));
+                    // film.AddResourceProperty(, genre.Resource, true);
+                    // film.AddLiteralProperty(UriFactory.Create(genreStr), g.CreateLiteralNode(genreStr), false);
+                    // film.AddResourceProperty(genreStr, g.CreateUriNode(genreStr), true);
+                    // genreClass.AddLiteralProperty(UriFactory.Create(genreStr), g.CreateLiteralNode(genreStr), true);
+                }
+            }
+
+            RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
+            rdfxmlwriter.Save(g, "HelloWorld.rdf");
+        }
+
+        public static void QueryFile()
+        {
+            IGraph g = new Graph();
+            // RdfXmlParser parser = new RdfXmlParser();
+            // parser.Load(g, "HelloWorld.rdf");
+
+            /*            //Load using a Filename
+                        FileLoader.Load(g, "HelloWorld.rdf");
+                        // dbopedia.org/resource
+                        //  ?movie <http://dbpedia.org/ontology/genre> ?genre .
+                        SparqlQueryParser qParser = new SparqlQueryParser();
+                        SparqlQuery query = qParser.ParseFromString(@"
+
+                            SELECT * WHERE {
+                                    ?movie a <http://dbpedia.org/resource/Albedo_One> .
+
+                                } LIMIT 100");
+
+                        var results = g.ExecuteQuery(query);*/
+
+            g.LoadFromFile("HelloWorld.rdf");
+            ISparqlDataset ds = new InMemoryDataset(g);
+            LeviathanQueryProcessor processor = new LeviathanQueryProcessor(ds);
+
+            SparqlQueryParser qParser = new SparqlQueryParser();
+            SparqlQuery query = qParser.ParseFromString(@"
+
+                SELECT * WHERE {
+                        ?movie a <http://dbpedia.org/ontology/Film> .
+                        ?movie <http://dbpedia.org/ontology/genre> ?genre      
+                    } LIMIT 100");
+
+            var results = processor.ProcessQuery(query);
+        }
+
+    }
+}
