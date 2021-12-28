6823baaaa841403f5c791f5557e4d6da55c3251f
SebastianAp27 <sebastianap271@gmail.com>
2021-12-28 19:18:20 +0200

    ontology-v0.0.0.0.0.0.0.0.0.1

---
+++ InitialDatasetService/InitialDatasetService/Controllers/OntologyController.cs
@@ -0,0 +1,20 @@
+﻿using InitialDatasetService.MicroServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InitialDatasetService.Controllers
+{
+    [ApiController]
+    [Route("/api1")]
+    public class OntologyController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+            OntologyInitializationService.initializeOntology();
+        }
+    }
+}
---
+++ InitialDatasetService/InitialDatasetService/MicroServices/OntologyInitializationService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+
+namespace InitialDatasetService.MicroServices
+{
+    public class OntologyInitializationService
+    {
+        public static void initializeOntology()
+        {
+
+
+            //Here we use an imaginary example file Ontology.rdf - substitute in an appropriate filename
+            IGraph g = new Graph();
+            FileLoader.Load(g, "Ontology.ttl");
+
+            //Get the Node representing the class of Interest
+            //Again we use an imaginary class URI, substitute in an appropriate URI
+            INode someClass = g.GetUriNode(new Uri("http://www.wade-ovi.org/Movie"));
+
+            //Note - GetUriNode returns null if no such URI exists, make sure to check for this or use
+            //CreateUriNode instead
+            if (someClass == null) return;
+            Console.WriteLine("ceva\n");
+            //Write out the Super Classes
+            INode subClassOf = g.CreateUriNode(new Uri(NamespaceMapper.RDFS + "subClassOf"));
+            foreach (Triple t in g.GetTriplesWithSubjectPredicate(someClass, subClassOf))
+            {
+                Console.WriteLine("Super Class: " + t.Object.ToString());
+            }
+            //Write out the Sub Classes
+            foreach (Triple t in g.GetTriplesWithPredicateObject(subClassOf, someClass))
+            {
+                Console.WriteLine("Sub Class: " + t.Subject.ToString());
+            }
+        }
+    }
+}
